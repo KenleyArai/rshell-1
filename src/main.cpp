e51952d4a1caf176f83bd7aec6c964c82b00e8cd
Angel Avendano <SparkTesla@github.com>
2014-10-20 17:07:41 -0700

    Updated main.cpp again

--- src/main.cpp
+++ src/main.cpp
@@ -8,84 +8,69 @@
 #include <errno.h>
 #include <stdlib.h>
 
-/*
-const int char_limit = 10000;
-
-char *String_Parsing(int argc, char *argv[]);
-*/
 using namespace std;
+
+//char **Tokenizer(string command);
 
 int main()
 {
-  int pid = fork();       // The return value when fork() is performed is a 0 or -1.
-                          // It is set into the variable pid (Processor ID).
+  string EXIT = "exit";		// exit() declaration
+
+  while(true)		       // Does not exit the program until exit() is invoked
+ {
+   string command;             // create the bash commands as strings
  
-  if(pid == -1)           // If pid equals to -1 then the fork failed.
-  {
-    perror("fork() failed completely");   // Prints an error message when fork() failed
+   cout << "$ ";	       // The prompt shows $ in the beginning
+   getline(cin,command);       // Get the input from user
+                     	       // It is set into the variable pid (Processor ID).
+   if(command.compare(EXIT) == 0)	 
+	exit(0);
+
+  //arg_rep[10000] =  Tokenizer(command);
+   char *cmd = new char[command.length() + 1]; 
+   strcpy(cmd, command.c_str());
+   char **argv = new char*[10000];
+   char *token;
+
+   token = strtok(cmd, " ");
     
-  }
+   int i = 0;
+   while( token != NULL)
+   {
+    // cout << token << endl;
 
-  else if(pid == 0)             /* If pid equals to 0 then the fork succeeded. All contents from the parent process is given to the new child process*/
-  {
-    string command;
-     
-    cout << "$ ";
-    getline(cin,command);
-    char *cmd = new char[command.length() + 1];
-    strcpy(cmd, command.c_str());
-    char **argv = new char*[100];
-    char *token;
+    argv[i] = token;
+    token = strtok(NULL, " "); 
+    i++;
+   }
+    	 
+   argv[i] = 0;
 
-    token = strtok(cmd, " ");
 
-    int i = 0;
-    while( token != NULL)
-    {
-      cout << token << endl;
+   int pid = fork();       // The return value when fork() is performed is a 0 or -1.
 
-      argv[i] = token;
-      token = strtok(NULL, " ");
-      i++;
-    }
-
-    argv[i] = 0;
-
-    execvp(argv[0], argv);
-   
-    cout << "Print: " << command << endl;
-
+   if(pid == -1)           // If pid equals to -1 then the fork failed.
+   {
+     perror("fork() failed completely");   // Prints an error message when fork() failed
     
-  //  cout << "$ ";
-
-   // cin.getline(cin, name);
-
-   // stcrcpy(name, cmd);
-
-    
-    
-   // execvp("ls", cmd);
-   // execvp("mkdir", argv2);
-   // execvp("g++", argv3);
-
-    cout << "i'm a child" << endl;  // prints out if fork succeeded then the child process is created
-
    }
 
-  else
-  {
-   wait(0);
-   cout << "I'm a parent." << endl;
+/* If pid equals to 0 then the fork succeeded. Contents from the parent process 
+   is given to the new child process*/
 
-  }
+   else if(pid == 0)     
+   { 
+	
+    execvp(argv[0], argv);
+   
+   }
 
+   else
+   {
+    wait(0);
+    cout << "I'm a parent." << endl; 
+   }
+ }
    return(0);
-
 }
 
-
-char *String_Parsing(int argc, char *argv[])
-{
-     
-
-}
