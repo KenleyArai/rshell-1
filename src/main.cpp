1956f0661ac82b1f7951edcae5fe5deb405ba835
Angel Avendano <SparkTesla@github.com>
2014-10-21 23:07:25 -0700

    Latest main.cpp update

--- src/main.cpp
+++ src/main.cpp
@@ -9,19 +9,20 @@
 #include <stdlib.h>
 #include <pwd.h>
 
-
 using namespace std;
 
+//bool Connector(char * tok);
+void Exec_VP(int char_limit, char**argv);
 
 int main()
 {
   string EXIT = "exit";		// exit() declaration
+  string command;                 // set bash commands as strings
 
   while(true)		           // Does not exit the program until exit() is invoked
  {
-   string command;                 // set bash commands as strings
    char * get_login = getlogin();  // set getlogin
-
+ 
    cout << get_login;		   // print login on the prompt
 
    int g = gethostname(get_login, 30);  // get hostname from login
@@ -35,31 +36,43 @@ int main()
     perror("No hostname");
    }
 
-   cout << "$ ";	       // The prompt shows $ in the beginning
+   cout << " $ ";	       // The prompt shows $ in the beginning
    getline(cin,command);       // Get the input from user
                      	       // It is set into the variable pid (Processor ID).
    if(command.compare(EXIT) == 0)	 
 	exit(0);
 
-   
-   char *cmd = new char[command.length() + 1]; 
+   int cmd_limit = command.length() + 1;
+   char *cmd = new char[cmd_limit]; 
+
    strcpy(cmd, command.c_str());
-   char **argv = new char*[command.length() + 1];
-   char *token;
+   char **argv = new char*[cmd_limit];
+   char * token;
+   //char *token2, *token3, *token4;
 
    token = strtok(cmd, " ");
+   //token2 = strtok(cmd, ";");
+   //token3 = strtok(cmd, "&");
+   //token4 = strtok(cmd, "|");
+   
     
    int i = 0;
-   
-   while( token != NULL )
+
+   while(token != NULL)
    {
     argv[i] = token;
-    token = strtok(NULL, " "); 
+    token = strtok(NULL, " ");
+
+   /* if(Connector(token2) == true)
+    {  
+     Exec_VP(i, argv);
+    }*/
     i++;
+    
    }
    
-   argv[i] = 0;	           //Sets the null pointer
-  
+   argv[i] = 0;	           //Sets the null pointer 
+
    int pid = fork();       // The return value when fork() is performed is a 0 or -1.
 
    if(pid == -1)           // If pid equals to -1 then the fork failed.
@@ -72,20 +85,40 @@ int main()
    is given to the new child process*/
 
    else if(pid == 0)     
-   {
-    //perror(";"); 
-    cout << "i'm a child" << endl;	
-    execvp(argv[0], argv); 
+   { 
+     execvp(argv[0], argv); 
    }
 
    else
    {
     wait(0);
-    cout << "I'm a parent." << endl; 
+    //cout << "I'm a parent." << endl; 
    }
  }
    return(0);
 }
 
 
+/*bool Connector(char * tok)
+{
+  if(tok == ";")
+  {
+    return(true);
+  }
+  else if(tok == "&&")
+  {
+    return true;
+  }
+  else if(tok == "||")
+  {
+    return true;
+  }
+ return false;
+}
 
+void Exec_VP(int char_limit, char**argv)
+{
+  execvp(argv[char_limit], argv);
+  return;
+}
+*/
